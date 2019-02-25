115002bde4b7dd416d2beddb14d544783f4c53d8
Marek Pikosz <marek.pikosz@gmail.com>
2019-02-25 22:27:52 +0100

    Divide and Conjugate methods added

--- complex.js
+++ complex.js
@@ -9,15 +9,29 @@ Complex.prototype.sum = (x, y) => {
     z = new Complex(sumReal, sumImaginary);
     return z;
 }
-Complex.prototype.sub = (x, y) => {
+Complex.prototype.subtract = (x, y) => {
     let subReal = x.real - y.real;
     let subImaginary = x.imaginary - y.imaginary;
     z = new Complex(subReal, subImaginary);
     return z;
 }
 Complex.prototype.multiply = (x, y) => {
-    let multiplyReal = x.real * y.real - (x.imaginary * y.imaginary);
-    let multiplyImaginary = x.real * y.imaginary + x.imaginary * y.real;
-    z = new Complex(multiplyReal, multiplyImaginary);
+    let mulReal = x.real * y.real - (x.imaginary * y.imaginary);
+    let mulImaginary = x.real * y.imaginary + x.imaginary * y.real;
+    z = new Complex(mulReal, mulImaginary);
+    return z;
+}
+Complex.prototype.divide = (x, y) => {
+    let con = y.conjugate();
+    console.log(con);
+    let up = x.multiply(x,con);
+    console.log(up);
+    let down = x.multiply(y, con);
+    console.log(down);
+    z = new Complex(up.real / down.real, up.imaginary / down.real);
+    return z;
+}
+Complex.prototype.conjugate = function() {
+    z = new Complex(this.real, - this.imaginary);
     return z;
 }
