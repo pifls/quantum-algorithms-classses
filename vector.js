c2413ee2752560d75fe722008ce7e2500f6c6bbe
Marek Pikosz <marek.pikosz@gmail.com>
2019-03-08 22:50:36 +0100

    vector - multiply by scalar

--- vector.js
+++ vector.js
@@ -4,9 +4,14 @@ function Vector(coefficients) {
     this.coefficients = coefficients;
     this.n = coefficients.length;
 
-    Vector.prototype.sum = (x, y) => {
+    Vector.prototype.sum = (v1, v2) => {
         let newCoefficients = []
-        x.coefficients.map((complex,index) => newCoefficients.push(complex.sum(x.coefficients[index],y.coefficients[index])));
-        return newCoefficients;
+        v1.coefficients.map((coefficient,index) => newCoefficients.push(coefficient.sum(coefficients[index],v2.coefficients[index])));
+        return new Vector(newCoefficients);
+    }
+    Vector.prototype.multiplyByScalar = (v, scalar) => {
+        let newCoefficients = []
+        v.coefficients.map((coefficient,index) => newCoefficients.push(coefficient.multiply(x.coefficients[index], scalar)));
+        return new Vector(newCoefficients);
     }
 }
